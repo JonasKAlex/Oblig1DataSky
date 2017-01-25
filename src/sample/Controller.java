53c8203c74a49e2e5422fe99225ca0a32b17c9cb
Jonas <s236328@stud.hioa.no>
2017-01-25 17:34:41 +0100

    Second

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,43 @@
 package sample;
 
-public class Controller {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable{
+
+    @FXML
+    private Label label;
+
+    @FXML
+    private Button button;
+
+    @FXML
+    void handleButtonAction(ActionEvent event)
+    {
+        try{
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("register.fxml"));
+            Parent root1 = (Parent)fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setTitle("Register");
+            stage.setScene(new Scene(root1));
+            stage.show();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+    @Override
+    public void initialize(URL url, ResourceBundle rb){
+
+    }
 }
